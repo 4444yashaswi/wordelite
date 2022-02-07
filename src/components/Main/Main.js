b178fc5b3e793be7a35d04a694828b1a1cb12df3
Abhishek Srivas <abhiautar123@gmail.com>
2022-02-08 02:17:40 +0530

    FEAT: event listener added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../Navbar/NavBar'
 import Grid from '../Grid/Grid'
 import Keyboard from '../Keyboard/Keyboard';
@@ -6,9 +6,19 @@ import './Main.css'
 
 
 const Main = () => {
+
+  const [word,setWord] = useState("");
+  // Listening Key Press
+  document.addEventListener('keypress', (e) => {
+    console.log(word);  // issue raised 
+    const temp = e.key;
+    setWord(word+temp);
+  });
+
   return <div className='Main-wrapper'>
       <Navbar />
       <Grid />
+      {(word)?<p style={{color:"white"}}>{word}</p>:""}
       <Keyboard />
   </div>;
 };
